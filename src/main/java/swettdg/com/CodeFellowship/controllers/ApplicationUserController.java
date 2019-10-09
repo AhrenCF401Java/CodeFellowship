74e6baed0f2e9fc71f5c16c86026e7d084d4e096
ahren <ahrenswett@gmail.com>
2019-10-09 13:54:26 -0700

    added userdetails page route login route sign in route forms and fields to sign up an login

--- src/main/java/swettdg/com/CodeFellowship/controllers/ApplicationUserController.java
+++ src/main/java/swettdg/com/CodeFellowship/controllers/ApplicationUserController.java
@@ -3,11 +3,15 @@ package swettdg.com.CodeFellowship.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.view.RedirectView;
 import swettdg.com.CodeFellowship.models.ApplicationUser;
 import swettdg.com.CodeFellowship.models.ApplicationUserRepository;
+
+import java.security.Principal;
 
 @Controller
 public class ApplicationUserController {
@@ -17,17 +21,25 @@ public class ApplicationUserController {
     @Autowired ApplicationUserRepository applicationUserRepository;
 
     @PostMapping("/signup")
-    public RedirectView createNewUser(String username, String password){
+    public RedirectView createNewUser(String username, String password, String firstName, String lastName, String dob, String bio){
 //        create user and salt/hash password
-        ApplicationUser u = new ApplicationUser(username,passwordEncoder.encode(password));
+        ApplicationUser u = new ApplicationUser(username, passwordEncoder.encode(password), firstName, lastName, dob, bio);
 //        save the user info in DB
         applicationUserRepository.save(u);
 //        send user back to the homepage
         return new RedirectView("/");
     }
 
+    @GetMapping("/userdetails/{username}")
+    public String getUserDetailsPage(@PathVariable Long id, Principal p, Model m){
+        m.addAttribute("name", p);
+        return "userdetails" + username;}
+
+    @GetMapping("/signup")
+    public String getSignUpPage(){ return "signup";};
+
     @GetMapping("/login")
-    public String getLoginPage() {
+    public String getLoginPage(){
         return "login";
     }
 
